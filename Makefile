9ef3a70c7bce78571e33e14450ce5f63afae0bb5
hiromaily <hiromaily2@gmail.com>
2018-02-11 12:36:30 +0100

    added server mode

--- Makefile
+++ Makefile
@@ -1,9 +1,21 @@
+# Build
+build:
+	go build -i -race -v -o ${GOPATH}/bin/gh ./cmd/
+
+# Sample for saying something in English.
 run:
 	go run cmd/*.go -msg "This is test speaking." -lang en
 
+# Sample for saying something in English without debug log.
 run2:
 	go run cmd/*.go -msg "This is test speaking." -lang en -log 2
 
-build:
-	go build -i -race -v -o ${GOPATH}/bin/gh ./cmd/
+# Sample for server mode.
+server:
+	go run cmd/*.go -server
+
+# Sample to post message to server by HTTPie
+say:
+	http POST http://localhost:8080/speak text="It's sunny day today."
+
 
--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
 	gglh "github.com/hiromaily/go-google-home"
 	lg "github.com/hiromaily/golibs/log"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 var (
-	message  = flag.String("msg", "", "Message to Google Home")
-	lang     = flag.String("lang", "en", "Language to speak")
-	server   = flag.Bool("server", false, "Run by server mode")
-	logLevel = flag.Int("log", 1, "Run by debug mode")
+	message    = flag.String("msg", "", "Message to Google Home")
+	lang       = flag.String("lang", "en", "Language to speak")
+	server     = flag.Bool("server", false, "Run by server mode")
+	serverPort = flag.Int("port", 8080, "Server por")
+	logLevel   = flag.Int("log", 1, "Run by debug mode")
 )
+
+type GHServer struct {
+	*gglh.GoogleHome
+}
+
+type Speak struct {
+	Text string `json:"text"`
+}
 
 func init() {
 	flag.Parse()
@@ -43,7 +60,7 @@ func main() {
 
 	// 3.server mode
 	if *server {
-
+		listen(gh)
 	} else {
 		// 4.speak something
 		err := gh.Speak(*message, *lang)
@@ -52,5 +69,100 @@ func main() {
 			return
 		}
 	}
-	//time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
+}
+
+func listen(gh *gglh.GoogleHome) {
+	stopCh := make(chan os.Signal)
+	signal.Notify(stopCh, os.Interrupt)
+
+	//server object
+	ghs := GHServer{}
+	ghs.GoogleHome = gh
+
+	http.HandleFunc("/speak", ghs.handler())
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", *serverPort), Handler: http.DefaultServeMux}
+	lg.Infof("Server start with port %d ...", *serverPort)
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			lg.Infof("listen: %s\n", err)
+			return
+		}
+	}()
+
+	<-stopCh // wait for SIGINT
+
+	lg.Info("Shutting down server...")
+	// shut down gracefully, but wait no longer than 5 seconds before halting
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+	lg.Info("Server gracefully stopped")
+
+}
+
+func (g *GHServer) handler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//check post or not
+		if r.Method != "POST" {
+			http.Error(w, "Method is not allowed.", http.StatusMethodNotAllowed)
+			return
+		}
+		//check parameter
+		r.ParseForm()
+		var said string
+		var err error
+		if v, ok := r.Form["text"]; ok {
+			said, err = g.speak(w, v[0])
+		} else {
+			//json
+			said, err = g.parseJson(w, r)
+		}
+		if err != nil {
+			return
+		}
+		lg.Infof("said: %s", said)
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, said)
+	}
+}
+
+func (g *GHServer) speak(w http.ResponseWriter, text string) (string, error) {
+	if text != "" {
+		err := g.Speak(text, *lang)
+		if err != nil {
+			http.Error(w, "It couldn't speak.", http.StatusInternalServerError)
+			lg.Errorf("gh.Speak() error:%v", err)
+			return "", err
+		}
+	} else {
+		http.Error(w, "Parameter is invalid.", http.StatusBadRequest)
+		lg.Error("gh.Speak() error: text is blank")
+		return "", fmt.Errorf("Parameter is invalid.")
+	}
+	return text, nil
+}
+
+func (g *GHServer) parseJson(w http.ResponseWriter, r *http.Request) (string, error) {
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		lg.Errorf("gh.parseJson() error:%v", err)
+		return "", err
+	}
+
+	var speak Speak
+	err = json.Unmarshal(b, &speak)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		lg.Errorf("gh.parseJson() error:%v", err)
+		return "", err
+	}
+
+	return g.speak(w, speak.Text)
 }
